f9a578798030872069640d43d57e52125320de03
lnchambers <lukenatchambers@gmail.com>
2017-12-02 18:16:03 -0700

    Add ability to generate difficulty

--- lib/generate.rb
+++ lib/generate.rb
@@ -6,6 +6,8 @@ class Generate
 
   def initialize
     @colors_easy = ["r", "b", "g", "y"]
+    @colors_medium = ["r", "b", "g", "y", "p"]
+    @colors_hard = ["r", "b", "g", "y", "p", "o"]
     @element = []
   end
 
@@ -15,4 +17,18 @@ class Generate
     end
     @element
   end
-end 
+
+  def medium
+    6.times do
+      @element << @colors_medium.sample
+    end
+    @element
+  end
+
+  def hard
+    8.times do
+      @element << @colors_hard.sample
+    end
+    @element
+  end
+end
